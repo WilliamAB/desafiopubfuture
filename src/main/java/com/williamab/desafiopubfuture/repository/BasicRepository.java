4383adfb37cfd9c098a8655f49e86418b6156a66
William Alberto Bertoldi <william.bertoldi@gmail.com>
2022-01-15 14:06:25 -0300

    Configurações para implementação dos testes unitários

--- src/main/java/com/williamab/desafiopubfuture/repository/BasicRepository.java
+++ src/main/java/com/williamab/desafiopubfuture/repository/BasicRepository.java
@@ -1,7 +1,7 @@
 package com.williamab.desafiopubfuture.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.williamab.desafiopubfuture.model.BasicEntity;
 
@@ -13,6 +13,6 @@ import com.williamab.desafiopubfuture.model.BasicEntity;
  * @param <E> uma entidade que estenda {@link BasicEntity}
  */
 @NoRepositoryBean
-public interface BasicRepository<E extends BasicEntity> extends PagingAndSortingRepository<E, Long> {
+public interface BasicRepository<E extends BasicEntity> extends JpaRepository<E, Long> {
 
 }
