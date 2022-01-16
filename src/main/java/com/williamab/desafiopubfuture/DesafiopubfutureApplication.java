928b7620ad0de8b92ab7a32718d4cdee1aec4202
William Alberto Bertoldi <william.bertoldi@gmail.com>
2022-01-16 17:09:19 -0300

    Configurações para documentação da API

--- src/main/java/com/williamab/desafiopubfuture/DesafiopubfutureApplication.java
+++ src/main/java/com/williamab/desafiopubfuture/DesafiopubfutureApplication.java
@@ -2,12 +2,24 @@ package com.williamab.desafiopubfuture;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @SpringBootApplication
 public class DesafiopubfutureApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafiopubfutureApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.components(new Components())
+				.info(new Info().title("Desafio PubFuture API"));
 	}
 
 }
